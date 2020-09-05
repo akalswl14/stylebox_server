8f8383cbe98dc68fe9d8de4c29081ccfb3935377
aeuna <4294psy@naver.com>
2020-09-06 03:59:13 +0900

    Add getQuestionOption

---
+++ src/types/resolvers/Question/getQuestionOption.ts
@@ -0,0 +1,26 @@
+import { queryField } from '@nexus/schema';
+
+export const getQuestionOption = queryField('getQuestionOption', {
+  type: 'Setting',
+  nullable: true,
+  list: true,
+  resolve: async (_, __, ctx) => {
+    try {
+      let settingQueryResult, QuestionOption;
+
+      settingQueryResult = await ctx.prisma.setting.findOne({
+        where: { id: 1 },
+        select: { QuestionOption: true },
+      });
+
+      QuestionOption = settingQueryResult
+        ? settingQueryResult.QuestionOption
+        : null;
+
+      return QuestionOption;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  },
+});
