619a4746a78e9b91d2379df0bfa9c3ef7cb58e61
Minji Ma <gnkryd12@gmail.com>
2020-09-16 15:50:10 +0900

    Delete old files

--- src/types/resolvers/Class/index.ts
+++ src/types/resolvers/Class/index.ts
@@ -1,2 +1,1 @@
 export * from "./createClass";
-export * from "./getAllClass";
--- src/types/resolvers/Product/index.ts
+++ src/types/resolvers/Product/index.ts
@@ -1,7 +1,1 @@
 export * from "./createProduct";
-export * from "./deleteProduct";
-export * from "./getAllProduct";
-export * from "./getAllProductbyTag";
-export * from "./getProduct";
-export * from "./toggleLikeProduct";
-export * from "./updateProduct";
