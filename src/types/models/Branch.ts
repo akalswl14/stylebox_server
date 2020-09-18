700685be7913e2fe443bfe7b54aff0e1e54d6f26
Minji Ma <gnkryd12@gmail.com>
2020-09-18 11:31:53 +0900

    Auto stash before merge of "master" and "origin/master"

--- src/types/models/Branch.ts
+++ src/types/models/Branch.ts
@@ -1,27 +1,25 @@
-import { objectType } from '@nexus/schema';
+import { objectType } from "@nexus/schema";
 
 export const Branch = objectType({
-  name: 'Branch',
+  name: "Branch",
   definition(t) {
     t.model.id();
     t.model.shopId();
     t.model.Shop();
     t.model.names();
-    t.model.logoUrl();
     t.model.phoneNumbers();
     t.model.products();
     t.model.address();
     t.model.googleMapUrl();
     t.model.longitude();
     t.model.latitude();
-    t.model.tags();
     t.model.createdAt();
     t.model.updatedAt();
   },
 });
 
 export const BranchName = objectType({
-  name: 'BranchName',
+  name: "BranchName",
   definition(t) {
     t.model.id();
     t.model.branchId();
