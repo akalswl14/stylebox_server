0977027eca6280349fa0bd51cc662bb951ef1544
aeuna <4294psy@naver.com>
2020-11-22 23:58:58 +0900

    Update OutPutModel and updateClassInfo, createPost and createTagInfo

--- src/types/resolvers/Management/Tag/createTagInfo.ts
+++ src/types/resolvers/Management/Tag/createTagInfo.ts
@@ -1,14 +1,14 @@
 import { arg, intArg, mutationField, stringArg } from "@nexus/schema";
 
 export const createTagInfo = mutationField("createTagInfo", {
-  type: "Boolean",
+  type: "TagIdInfo",
   args: {
     tagName: stringArg({ required: true }),
     tagCategory: arg({ type: "Category", required: true }),
     tagImage: stringArg({ nullable: true }),
     classId: intArg({ required: true }),
   },
-  nullable: false,
+  nullable: true,
   resolve: async (_, args, ctx) => {
     try {
       const { tagName, tagCategory, tagImage, classId } = args;
@@ -27,7 +27,7 @@ export const createTagInfo = mutationField("createTagInfo", {
           where: { id: classId, category: tagCategory },
           select: { names: { where: { lang }, select: { word: true } } },
         });
-        if (!classNameResult) return false;
+        if (!classNameResult) return null;
         if (classNameResult[0].names[0].word === tagName) {
           isClass = true;
         }
@@ -36,20 +36,33 @@ export const createTagInfo = mutationField("createTagInfo", {
       let queryResult = await ctx.prisma.tag.create({
         data: {
           category: tagCategory,
-          tagImage,
           isClass,
           isRecommendation: 0,
           Class: { connect: { id: classId } },
           names: { create: { lang, word: tagName } },
         },
+        select: {
+          id: true,
+        },
       });
 
-      if (!queryResult) return false;
+      if (!queryResult) return null;
 
-      return true;
+      if (tagImage) {
+        const s3ImageKey = "Tag/" + queryResult.id + "/" + tagImage;
+
+        let createImage = await ctx.prisma.tag.update({
+          where: { id: queryResult.id },
+          data: { tagImage: s3ImageKey },
+        });
+
+        if (!createImage) return null;
+      }
+
+      return queryResult ? { tagId: queryResult.id } : null;
     } catch (e) {
       console.log(e);
-      return false;
+      return null;
     }
   },
 });
