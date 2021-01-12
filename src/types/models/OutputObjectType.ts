14cb229496adc7589cea2c879d100c05d016c478
aeuna <42725903+aeuna@users.noreply.github.com>
2021-01-12 19:10:27 +0900

    Revert "[APP API] 검색창 지역필터 개선"

--- src/types/models/OutputObjectType.ts
+++ src/types/models/OutputObjectType.ts
@@ -182,15 +182,6 @@ export const levelCategoryOption = objectType({
   },
 });
 
-export const levelLocationOption = objectType({
-  name: "levelLocationOption",
-  definition(t) {
-    t.int("classId");
-    t.string("className");
-    t.field("subTags", { type: "TagThumbnail", list: true });
-  },
-});
-
 export const EventDetail = objectType({
   name: "EventDetail",
   definition(t) {
